3cce1c181eb410b1dd88314832d5f7c97c0f954e
Anindit Karmakar <aninditkarmakar@gmail.com>
2021-02-23 17:00:26 -0800

    network idle

--- src/ProductPage.ts
+++ src/ProductPage.ts
@@ -75,7 +75,7 @@ export class ProductPage {
 
 	async navigate() {
 		try {
-			await this._page.goto(this._config.url);
+			await this._page.goto(this._config.url, { waitUntil: 'networkidle' });
 			this._logger.info(`[${this._id}] Loaded website.`);
 		} catch (err: any) {
 			throw new ProductPageError(`Error navigating to website.`, err);
